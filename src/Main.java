423e23c9af9f854872a14f695e453650063275a5
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2025-02-22 16:08:58 +0530

    adding reverse function of a given set of integers

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        checkCount("Bogahagoda");
+      /*  checkCount("Bogahagoda");
     }
     public static void checkCount (String typeName){
         HashMap<Character, Integer> checkChar = new HashMap<Character, Integer>();
@@ -17,6 +18,19 @@ public class Main {
                 checkChar.put(c,1);
             }
         }
-        System.out.println(checkChar);
+        System.out.println(checkChar);*/
+
+    // check reverse functionality of given set of integers
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the number");
+        int num = sc.nextInt();
+        int rev = 0;
+
+        while(num != 0){
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        System.out.println(rev);
     }
 }
